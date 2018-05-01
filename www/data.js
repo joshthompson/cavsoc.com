5cf1111a4b35c161df3eae7f1476a29e442fca22
Josh Thompson <josh3276@gmail.com>
2018-05-01 20:55:32 +0200

    Initial Commit

---
+++ www/data.js
@@ -0,0 +1,59 @@
+// This is an array of the different backgrounds and related properties
+var backgrounds = [
+	{
+		src: "./images/projects/iou-coming-soon.jpg",
+		url: "https://soundcloud.com/kai-jones"
+	},
+	{
+		src: "./images/projects/lydia-maddix.jpg",
+		url: "https://youtu.be/_RZSIvAd7Z8"
+	},
+	{
+		src: "./images/projects/kai-jones.jpg",
+		url: "https://youtu.be/X2C-tnIdgHI"
+	},
+	{
+		src: "./images/projects/mr-litch.jpg",
+		url: "https://www.youtube.com/watch?v=EwssUXKwA30"
+	},
+	{
+		src: "./images/projects/kai-jones-2.jpg",
+		url: "https://vimeo.com/171730356"
+	},
+	{
+		src: "./images/projects/ebonie-g-2.jpg",
+		url: "https://vimeo.com/156100656"
+	},
+	{
+		src: "./images/projects/sabrina.jpg",
+		url: "https://vimeo.com/149508305"
+	},
+	{
+		src: "./images/projects/banana-hands.jpg",
+		url: "https://youtu.be/-nYbuVmZ8yM"
+	},
+	{
+		src: "./images/projects/topsxnina.jpg",
+		url: "https://vimeo.com/150074109"
+	},
+	{
+		src: "./images/projects/stella-mann-college.jpg",
+		url: "http://www.stellamanncollege.co.uk/"
+	},
+	{
+		src: "./images/projects/4real.jpg",
+		url: "https://vimeo.com/149507921"
+	},
+	{
+		src: "./images/projects/ws-studio.jpg",
+		url: "http://www.wssfashion.com/collections"
+	},
+	{
+		src: "./images/projects/ebonie-g.jpg",
+		url: "https://www.youtube.com/watch?v=ELdbhSKiRo4"
+	},
+	{
+		src: "./images/projects/mc-threat.jpg",
+		url: "https://www.youtube.com/watch?v=RFYOg56RuKk"
+	}
+];
---
+++ www/main.js
@@ -0,0 +1,65 @@
+(function() {
+
+	// This function resizes the background block so that it always stretched to match the bottom of the screen
+	var resize = function() {
+		$("#background").css({
+			height: $(window).height() - $("footer").height(),
+			marginBottom: -$(window).height()
+		});
+		$("#nav-left, #nav-right").css({
+			top: $(window).height() / 2 - $("#nav-left").height() / 2
+		});
+	};
+	resize();
+	$(window).resize(resize);
+
+	// Change page
+	window.gotoPage = function(page) {
+		if (page !== window.currentPage) {
+			window.currentPage = page;
+			$("[data-page].active").removeClass("active").addClass("page-exit");
+			$("[data-page='" + page + "']").addClass("active page-enter").scrollTop(0);
+			setTimeout(function() { $("[data-page]").removeClass("page-exit page-enter"); }, 1000);
+		}
+	};
+
+	// Preload images
+	(function preloadImages(img) {
+		if (typeof backgrounds[img] == "object") {
+			$("<img src='" + backgrounds[img]["src"] + "' />").width(0).height(0).load(function() {
+				preloadImages(img + 1);
+			});
+		}
+	})(1);
+
+	// Change background
+	var background_timer = 5; // Number of seconds
+	var background_update_set_timeout = null;
+	function backgroundUpdate(direction) {
+		var img = $("#background").is("[data-img]") ? (parseInt($("#background").attr("data-img")) + direction) % backgrounds.length : 0;
+		img = img < 0 ? backgrounds.length + img : img;
+		$("#background").attr("data-img", img);
+		$("#background").css({backgroundImage: "url('" + backgrounds[img]["src"] + "')"});
+		clearTimeout(background_update_set_timeout);
+		background_update_set_timeout = setTimeout(backgroundUpdate, background_timer * 1000, 1);
+	}
+	backgroundUpdate(1);
+	$("#nav-left").click(function() {
+		backgroundUpdate(-1);
+	});
+	$("#nav-right").click(function() {
+		backgroundUpdate(1);
+	});
+
+	// Navigate to page
+	$("#background").click(function(event) {
+		if ($(event.target).is("[href], .link") || $(event.target).closest("[href], .link").length) {
+			return false;
+		}
+		var img = $("#background").is("[data-img]") ? parseInt($("#background").attr("data-img")) : 0;
+		window.open(backgrounds[img]["url"], "_blank");
+	});
+
+})();
+
+var page
