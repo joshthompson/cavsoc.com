8dbbdfd7f77d91fa2ec7904425cff88d2219fa69
Joseph Madden <joe@cavsoc.com>
2018-12-04 17:11:43 +0000

    Small Biz Sat
    Tweaks to project page

--- www/projects/smallbizsat.php
+++ www/projects/smallbizsat.php
@@ -18,46 +18,27 @@
 
 			<div class="project-section">
 				<div class="project-section-content">
-					<h4>The campaign trail</h4>
-					<p>Our own Joseph Madden has been proudly photographing Small Business Saturday’s events for the past two years, from their annual Small Awards, celebrating the best and brightest small businesses; to the Bus Tour, which delivers free business mentoring sessions for small businesses up and down the country. This involvement has recently brought us into the political orbit of Whitehall...</p>
-				</div>
-				<div class="project-section-content">
-					<img class="project-image" src="/images/work/smallbizsat/smallbizsat-2.jpg" />
-				</div>
-			</div>
-
-			<div class="project-section">
-				<div class="project-section-content">
-					<img class="project-image" src="/images/work/smallbizsat/smallbizsat-3.jpg" />
-				</div>
-				<div class="project-section-content">
 					<img class="project-image" src="/images/work/smallbizsat/smallbizsat-4.jpg" />
 				</div>
-			</div>
-
-			<div class="project-section">
 				<div class="project-section-content">
-					<img class="project-image" src="/images/work/smallbizsat/smallbizsat-5.jpg" />
-				</div>
-				<div class="project-section-content">
-					<h4>House of Lords</h4>
-					<p>The Small Biz 100 - a list of the best small businesses in the UK - were chosen to attend the House of Lords to shine a spotlight on the work they do. We photographed speeches from The Rt. Hon. Lord Young of Graffham, as well as the Secretary of State for Business, Energy & Industrial Strategy, Greg Clark MP.</p>
+					<h4>The campaign trail</h4>
+					<p>Our own Joseph Madden has been proudly photographing Small Business Saturday’s events for the past two years, from their Bus Tour, which delivers free business mentoring sessions for small businesses up and down the country; to the annual Small Awards, celebrating the best and brightest small businesses. This involvement has recently brought us into the political orbit of Whitehall...</p>
 				</div>
 			</div>
 
 			<div class="project-section">
 				<div class="project-section-content">
-					<img class="project-image" src="/images/work/smallbizsat/smallbizsat-6.jpg" />
+					<img class="project-image" src="/images/work/smallbizsat/smallbizsat-19.jpg" />
 				</div>
 				<div class="project-section-content">
-					<img class="project-image" src="/images/work/smallbizsat/smallbizsat-7.jpg" />
+					<img class="project-image" src="/images/work/smallbizsat/smallbizsat-3.jpg" />
 				</div>
 			</div>
 
 			<div class="project-section">
 				<div class="project-section-content">
 					<h4>Downing Street</h4>
-					<p>25 days, 30 locations, and 3000 miles later, the Small Business Saturday bus pulled into Downing Street for the final stop on its cross-country tour. As part of a reception for the UK’s best small businesses, we recorded coverage of Chancellor of the Exchequer Philip Hammond, Secretary of State for Health & Social Care Matt Hancock, and Minister for Small Business Kelly Tolhurst.</p>
+					<p>After a mammoth 25 days, 30 locations, and 3000 miles, the Small Business Saturday bus pulled into Downing Street for the final stop on its cross-country tour, and we were there to greet it. As part of a reception for the UK’s best small businesses, we recorded coverage of Chancellor of the Exchequer Philip Hammond, Secretary of State for Health & Social Care Matt Hancock, and Minister for Small Business Kelly Tolhurst.</p>
 				</div>
 				<div class="project-section-content">
 					<img class="project-image" src="/images/work/smallbizsat/smallbizsat-8.jpg" />
@@ -89,26 +70,55 @@
 				</div>
 			</div>
 
+
 			<div class="project-section">
+				<div class="project-section-content">
+					<h4>House of Lords</h4>
+					<p>The Small Biz 100 - a list of the best small businesses in the UK - were chosen to attend the House of Lords to shine a spotlight on the work they do. We photographed speeches from The Rt. Hon. Lord Young of Graffham, as well as the Secretary of State for Business, Energy & Industrial Strategy, Greg Clark MP.</p>
+				</div>
+				<div class="project-section-content">
+					<img class="project-image" src="/images/work/smallbizsat/smallbizsat-6.jpg" />
+				</div>
+			</div>
+
+			<div class="project-section">
+				<div class="project-section-content">
+					<img class="project-image" src="/images/work/smallbizsat/smallbizsat-5.jpg" />
+				</div>
+				<div class="project-section-content">
+					<img class="project-image" src="/images/work/smallbizsat/smallbizsat-7.jpg" />
+				</div>
+			</div>
+
+			<div class="project-section">
+				<div class="project-section-content">
+					<img class="project-image" src="/images/work/smallbizsat/smallbizsat-13.jpg" />
+				</div>
 				<div class="project-section-content">
 					<h4>Grassroots</h4>
 					<p>Small Business Saturday UK is a grassroots, non-commercial campaign, which highlights small business success and encourages consumers to 'shop local' and support small businesses in their communities. The day itself takes place on the first Saturday in December each year, but the campaign aims to have a lasting impact on small businesses.</p>
 				</div>
+			</div>
+<!--
+			<div class="project-section">
 				<div class="project-section-content">
-					<img class="project-image" src="/images/work/smallbizsat/smallbizsat-13.jpg" />
+					<p>"Something nice"</p>
+					<p>Michelle Ovens MBE, Managing Director</p>
+				</div>
+				<div class="project-section-content">
+					<img class="project-image" src="/images/work/smallbizsat/smallbizsat-14.jpg" />
 				</div>
 			</div>
 
 			<div class="project-section">
 				<div class="project-section-content">
-					<img class="project-image" src="/images/work/smallbizsat/smallbizsat-14.jpg" />
+					<img class="project-image" src="/images/work/smallbizsat/smallbizsat-17.jpg" />
 				</div>
 				<div class="project-section-content">
-					<p>"Something nice"</p>
-					<p>Michelle Ovens MBE, Managing Director</p>
+					<img class="project-image" src="/images/work/smallbizsat/smallbizsat-18.jpg" />
 				</div>
 			</div>
-
+-->
 			<div class="project-section">
 				<div class="project-section-content">
 					<p>Client: <a href="https://smallbusinesssaturdayuk.com/" target="_blank">Small Business Saturday</a></p>
