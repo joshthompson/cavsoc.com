46471a521e41c63b5e63e458cc95d50412caf26b
Joseph Madden <joe@cavsoc.com>
2018-12-04 19:00:00 +0000

    Arrival
    Test project page

---
+++ www/projects/arrival.php
@@ -0,0 +1,130 @@
+<? $page_title = "Arrival"; ?>
+<? require "../include/top.tpl"; ?>
+
+<div class="active content-page">
+	<div class="page-content">
+
+		<h2 class="cavsoc-heading"><em>Arrival</em></h2>
+
+		<div class="project-intro">
+			<div>Documentary and Internal Communications</div>
+		</div>
+
+		<div class="project">
+
+			<div class="project-main">
+				<img src="/images/work/arrival/arrival-1.jpg" />
+			</div>
+
+			<div class="project-section">
+				<div class="project-section-content">
+					<h4>From Russia With Footage</h4>
+					<p>Our first major international shoot came courtesy of Arrival, a technology company aiming to change the world through building smart and electric vehicles. Having produced Arrival’s previous six internal communications films, we already had a strong understanding of their business, making us a good fit for the St. Petersburg project. Over the course of a week we explored their software hub in St. Petersburg, Russia, to get a first glimpse at what the future looks like.</p>
+				</div>
+				<div class="project-section-content">
+					<img class="project-image" src="/images/work/arrival/arrival-2.jpg" />
+				</div>
+			</div>
+
+			<div class="project-section">
+				<div class="project-section-content">
+					<img class="project-image" src="/images/work/arrival/arrival-3.jpg" />
+				</div>
+				<div class="project-section-content">
+					<img class="project-image" src="/images/work/arrival/arrival-4.jpg" />
+				</div>
+			</div>
+
+			<div class="project-section">
+				<div class="project-section-content">
+					<img class="project-image" src="/images/work/arrival/arrival-5.jpg" />
+				</div>
+				<div class="project-section-content">
+					<h4>The black box</h4>
+					<p>While much of Arrival’s core operations take place in the UK, their main software arm is based in laboratories on the outskirts St. Petersburg. Described as the company’s ‘black box’, the St. Petersburg team routinely wow their British counterparts with technological breakthroughs despite very few colleagues knowing what it is, exactly, that they’re working on.</p>
+
+					<p>Our aim was to shine a light on the work they do, and produce a 4-part documentary series for internal consumption to give the rest of the business a deeper understanding of how the advances in software development tie in to the rest of the business’ success.</p>
+				</div>
+			</div>
+
+			<div class="project-section">
+				<div class="project-section-content">
+					<img class="project-image" src="/images/work/arrival/arrival-6.jpg" />
+				</div>
+				<div class="project-section-content">
+					<img class="project-image" src="/images/work/arrival/arrival-7.jpg" />
+				</div>
+			</div>
+
+			<div class="project-section">
+				<div class="project-section-content">
+					<h4>International business</h4>
+					<p>Producing content overseas always presents its own series of unique challenges, and this was no exception. Filming by-laws, creative styles, and working cultures vary massively from country to country, and it’s easy for productions to trip up even when they think they’ve accounted for these variables.</p>
+
+					<p>We counter this by being overly prepared, to the point of being excessive, and allowing additional time for communications. However, all the best groundwork and foresight is redundant without the autonomy and flexibility to change tack when the shoot demanded it. We were fortunate enough to have a client who gave us exactly freedom and remit.</p>
+				</div>
+				<div class="project-section-content">
+					<img class="project-image" src="/images/work/arrival/arrival-8.jpg" />
+				</div>
+			</div>
+
+			<div class="project-section">
+				<div class="project-section-content">
+					<img class="project-image" src="/images/work/arrival/arrival-9.jpg" />
+				</div>
+				<div class="project-section-content">
+					<h4>Local knowledge</h4>
+					<p>As luck would have it, we had a Russian director on our books, circumventing the need for a translator or complicated business visas. We hired a local production team to provide kit and technical crew, and sent Xenia to direct them and oversee the shoot. Xenia’s direction helped us ensure a style consistent with our previous work for Arrival, while having access to a local crew massively reduced logistical challenges and provided an expertise we would have suffered without.</p>
+				</div>
+			</div>
+
+			<div class="project-section">
+				<div class="project-section-content">
+					<img class="project-image" src="/images/work/arrival/arrival-10.jpg" />
+				</div>
+				<div class="project-section-content">
+					<img class="project-image" src="/images/work/arrival/arrival-11.jpg" />
+				</div>
+			</div>
+
+			<div class="project-section">
+				<div class="project-section-content">
+					<h4>Post-production</h4>
+					<p>Working with 4K proxies on long-form interviews recorded in a foreign language was never going to be an easy task, and we reimagined our whole post-production workflow based on the challenges we faced while editing this. Our director and producer created a paper-edit far more comprehensively than a typical project would require us to, and subtitled the complete interview rushes so that our editor (who very unreasonably doesn’t speak Russian!) could form a narrative.</p>
+				</div>
+				<div class="project-section-content">
+					<img class="project-image" src="/images/work/arrival/arrival-12.jpg" />
+				</div>
+			</div>
+
+			<div class="project-section">
+				<div class="project-section-content">
+					<img class="project-image" src="/images/work/arrival/arrival-13.jpg" />
+				</div>
+				<div class="project-section-content">
+					<p>"Testimonial"</p>
+					<p>Dan Hauck, Chief of Marketing, ARRIVAL</p>
+				</div>
+			</div>
+
+			<div class="project-section">
+				<div class="project-section-content">
+					<p>Client: <a href="https://arrival.com/" target="_blank">ARRIVAL</a></p>
+					<p>Director: <a href="https://vimeo.com/xeniazubova" target="_blank">Xenia Zubova</a></p>
+					<p>Producer: <a href="http://www.josephmadden.co.uk/" target="_blank">Joseph Madden</a></p>
+					<p>Production Manager: Arsenio Outekhine</p>
+					<p>Director of Photography: Igor Pogodin</p>
+					<p>Camera Assistant: Gleb Simonaiz</p>
+					<p>Sound Recordist: Alexey Kulkov</p>
+					<p>Editor: Jonny Magowan</p>
+					<p>Top image provided courtesy of ARRIVAL.</p>
+				</div>
+				<div class="project-section-content">
+					<img class="project-image" src="/images/work/arrival/arrival-14.jpg" />
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+
+<? require "../include/bottom.tpl"; ?>
