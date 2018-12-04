f6565335456f5f5f94556caaa5739fe3f3375b7c
Joseph Madden <joe@cavsoc.com>
2018-12-04 15:39:47 +0000

    Small Biz Sat
    New project page

---
+++ www/projects/smallbizsat.php
@@ -0,0 +1,125 @@
+<? $page_title = "Small Business Saturday"; ?>
+<? require "../include/top.tpl"; ?>
+
+<div class="active content-page">
+	<div class="page-content">
+
+		<h2 class="cavsoc-heading"><em>Small Business</em> Saturday</h2>
+
+		<div class="project-intro">
+			<div>Press Photography</div>
+		</div>
+
+		<div class="project">
+
+			<div class="project-main">
+				<img src="/images/work/smallbizsat/smallbizsat-1.jpg" />
+			</div>
+
+			<div class="project-section">
+				<div class="project-section-content">
+					<h4>The campaign trail</h4>
+					<p>Our own Joseph Madden has been proudly photographing Small Business Saturday’s events for the past two years, from their annual Small Awards, celebrating the best and brightest small businesses; to the Bus Tour, which delivers free business mentoring sessions for small businesses up and down the country. This involvement has recently brought us into the political orbit of Whitehall...</p>
+				</div>
+				<div class="project-section-content">
+					<img class="project-image" src="/images/work/smallbizsat/smallbizsat-2.jpg" />
+				</div>
+			</div>
+
+			<div class="project-section">
+				<div class="project-section-content">
+					<img class="project-image" src="/images/work/smallbizsat/smallbizsat-3.jpg" />
+				</div>
+				<div class="project-section-content">
+					<img class="project-image" src="/images/work/smallbizsat/smallbizsat-4.jpg" />
+				</div>
+			</div>
+
+			<div class="project-section">
+				<div class="project-section-content">
+					<img class="project-image" src="/images/work/smallbizsat/smallbizsat-5.jpg" />
+				</div>
+				<div class="project-section-content">
+					<h4>House of Lords</h4>
+					<p>The Small Biz 100 - a list of the best small businesses in the UK - were chosen to attend the House of Lords to shine a spotlight on the work they do. We photographed speeches from The Rt. Hon. Lord Young of Graffham, as well as the Secretary of State for Business, Energy & Industrial Strategy, Greg Clark MP.</p>
+				</div>
+			</div>
+
+			<div class="project-section">
+				<div class="project-section-content">
+					<img class="project-image" src="/images/work/smallbizsat/smallbizsat-6.jpg" />
+				</div>
+				<div class="project-section-content">
+					<img class="project-image" src="/images/work/smallbizsat/smallbizsat-7.jpg" />
+				</div>
+			</div>
+
+			<div class="project-section">
+				<div class="project-section-content">
+					<h4>Downing Street</h4>
+					<p>25 days, 30 locations, and 3000 miles later, the Small Business Saturday bus pulled into Downing Street for the final stop on its cross-country tour. As part of a reception for the UK’s best small businesses, we recorded coverage of Chancellor of the Exchequer Philip Hammond, Secretary of State for Health & Social Care Matt Hancock, and Minister for Small Business Kelly Tolhurst.</p>
+				</div>
+				<div class="project-section-content">
+					<img class="project-image" src="/images/work/smallbizsat/smallbizsat-8.jpg" />
+				</div>
+			</div>
+
+			<div class="project-section">
+				<div class="project-section-content">
+					<img class="project-image" src="/images/work/smallbizsat/smallbizsat-9.jpg" />
+				</div>
+				<div class="project-section-content">
+					<img class="project-image" src="/images/work/smallbizsat/smallbizsat-10.jpg" />
+				</div>
+			</div>
+
+			<div class="project-section">
+				<div class="project-section-content">
+					<img class="project-image" src="/images/work/smallbizsat/smallbizsat-11.jpg" />
+				</div>
+				<div class="project-section-content">
+					<h4>City Hall</h4>
+					<p>Showing his continued commitment to Small Business Saturday and having vowed “to be London’s  most pro-business mayor”, we photographed the Mayor of London Sadiq Khan with the director of Small Business Saturday, Michelle Ovens MBE.</p>
+				</div>
+			</div>
+
+			<div class="project-section">
+				<div class="project-section-content">
+					<img class="project-image" src="/images/work/smallbizsat/smallbizsat-12.jpg" />
+				</div>
+			</div>
+
+			<div class="project-section">
+				<div class="project-section-content">
+					<h4>Grassroots</h4>
+					<p>Small Business Saturday UK is a grassroots, non-commercial campaign, which highlights small business success and encourages consumers to 'shop local' and support small businesses in their communities. The day itself takes place on the first Saturday in December each year, but the campaign aims to have a lasting impact on small businesses.</p>
+				</div>
+				<div class="project-section-content">
+					<img class="project-image" src="/images/work/smallbizsat/smallbizsat-13.jpg" />
+				</div>
+			</div>
+
+			<div class="project-section">
+				<div class="project-section-content">
+					<img class="project-image" src="/images/work/smallbizsat/smallbizsat-14.jpg" />
+				</div>
+				<div class="project-section-content">
+					<p>"Something nice"</p>
+					<p>Michelle Ovens MBE, Managing Director</p>
+				</div>
+			</div>
+
+			<div class="project-section">
+				<div class="project-section-content">
+					<p>Client: <a href="https://smallbusinesssaturdayuk.com/" target="_blank">Small Business Saturday</a></p>
+					<p>Photographer: <a href="http://www.josephmadden.co.uk/" target="_blank">Joseph Madden</a></p>
+				</div>
+				<div class="project-section-content">
+					<img class="project-image" src="/images/work/smallbizsat/smallbizsat-15.jpg" />
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+
+<? require "../include/bottom.tpl"; ?>
